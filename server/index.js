0ecbae49032d77855ea5b34faee8ac938ed1dfbb
mengel142 <matthew.engel88@gmail.com>
2020-09-16 09:26:07 -0400

    delete test code

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
-const pool = require('../database/index.js');
 const router = require('./router');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
