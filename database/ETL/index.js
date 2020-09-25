7ad2e5f7267ed516af21a7836ed7ff7b2ada2026
mengel142 <matthew.engel88@gmail.com>
2020-09-25 12:31:15 -0400

    remove personal info

--- database/ETL/index.js
+++ database/ETL/index.js
@@ -1,16 +1,13 @@
 const { Pool, Client } = require('pg');
-
+require('dotenv').config();
 
 
 var pool = new Pool({
-    user: 'postgres',
-    host: '18.189.170.255',
-    database: 'greenfield_reviews',
-    password: 'pawscat',
-    port: 5432
-    // idleTimeoutMillis: 30000,
-    // connectionTimeoutMillis: 2000,
-    // max: 20
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
 })
 
 module.exports = pool;
